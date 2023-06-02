6c703dd97216b2818fd8e9ea7726cba05b11d839
Hayden Housen <hayden@haydenhousen.com>
2023-06-02 11:13:21 -0700

    Add Inter font & default to dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./layouts/**/*.html'],
   darkMode: 'class',
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
